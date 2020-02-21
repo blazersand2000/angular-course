517b903a94ba33c82018117aca4b0b82ab3d394e
Andrew <blazersand2000@yahoo.com>
2020-02-20 19:38:28 -0500

    Finished section 19

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -24,8 +25,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.url).subscribe(recipes => {
-      this.recipeService.setRecipes(recipes);
-    });
+    return this.http
+      .get<Recipe[]>(this.url)
+        .pipe(map(recipes => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
   }
 }
